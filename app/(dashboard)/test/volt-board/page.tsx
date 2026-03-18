fb34fe57eb9cbba93dedfcfc6cbdc642aca029ca
Yağız <yagizozturk@gmail.com>
2026-03-18 16:51:53 +0300

    refactor and test page

---
+++ app/(dashboard)/test/volt-board/page.tsx
@@ -0,0 +1,15 @@
+import VoltBoard from "@/components/volt-board/volt-board";
+
+export default function VoltBoardPage() {
+  return (
+    <div className="container mx-auto max-w-3xl p-8">
+      <VoltBoard
+        sourceId="test-board"
+        mode="opening"
+        moves="e2e4 e7e5 g1f3 b8c6"
+        width={500}
+        height={500}
+      />
+    </div>
+  );
+}
