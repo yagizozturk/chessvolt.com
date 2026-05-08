72c6fa8e0b342398560ea94a6bb33a22527e66d8
Yağız <yagizozturk@gmail.com>
2026-05-08 17:13:55 +0300

    tts fix

--- features/openings/components/opening-board-card.tsx
+++ features/openings/components/opening-board-card.tsx
@@ -51,7 +51,7 @@ export function OpeningBoardCard({
               <BookOpen />
               <span>{variantCount}</span>
             </Badge>
-            <Button variant="default">Play</Button>
+            <Button variant="voltOutline">Play</Button>
           </div>
         </div>
       </Link>
