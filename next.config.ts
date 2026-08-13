7ee40f491ce5e205c9cf9bef9c1e10372b05f978
aypupen <aypupen@yahoo.com>
2026-08-13 19:24:36 +0300

    Sentry exception handling and logging architecture added. Because it's not possible to collect all sentry files into a sub folder, files are located mainly in the root folder of the project.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: !process.env.CI,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  widenClientFileUpload: true,
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
+  enableLogs: true,
+  integrations: [
+    Sentry.consoleLoggingIntegration({
+      levels: ["log", "warn", "error"],
+    }),
+  ],
+});
